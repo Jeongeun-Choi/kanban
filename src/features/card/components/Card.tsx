aa48f941420a2bb07abace31e702469ea2392f5b
Jeongeun-Choi <cje3080@gmail.com>
2026-01-10 22:13:58 +0900

    feat(card): Card 상세 정보 화면 구현

--- src/features/card/components/Card.tsx
+++ src/features/card/components/Card.tsx
@@ -1,23 +1,31 @@
+import { useState } from "react";
 import { FaTrash } from "react-icons/fa";
 import { CardContainer, CardDeadline, CardTitle } from "../styles/styled";
+import type { Card as CardType } from "../../../shared/types/kanban";
+import CardDetailModal from "./CardDetailModal";
 
-interface CardProps {
-  id: string;
-  title: string;
-  description?: string;
-  due_date?: string;
-}
+type CardProps = CardType;
 
-export default function Card({ title, due_date }: CardProps) {
+export default function Card(card: CardProps) {
+  const [isDetailOpen, setIsDetailOpen] = useState(false);
+
   return (
-    <CardContainer>
-      <CardTitle>
-        <span>{title}</span>
-        <button>
-          <FaTrash />
-        </button>
-      </CardTitle>
-      <CardDeadline>{due_date}</CardDeadline>
-    </CardContainer>
+    <>
+      <CardContainer onClick={() => setIsDetailOpen(true)}>
+        <CardTitle>
+          <span>{card.title}</span>
+          <button
+            onClick={(e) => {
+              e.stopPropagation();
+              // Todo: Delete card
+            }}
+          >
+            <FaTrash />
+          </button>
+        </CardTitle>
+        <CardDeadline>{card.due_date}</CardDeadline>
+      </CardContainer>
+      <CardDetailModal card={card} open={isDetailOpen} onClose={() => setIsDetailOpen(false)} />
+    </>
   );
 }
