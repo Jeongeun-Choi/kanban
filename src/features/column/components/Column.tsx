936b2c8d87253170d38107a107bf86e6782291e8
Jeongeun-Choi <cje3080@gmail.com>
2026-01-10 17:32:14 +0900

    feat(column): delete column 기능 구현

--- src/features/column/components/Column.tsx
+++ src/features/column/components/Column.tsx
@@ -8,6 +8,8 @@ import { FaRegEdit, FaTrash } from "react-icons/fa";
 
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { updateColumn } from "../api/patchColumn";
+import Modal from "../../../shared/components/Modal";
+import { deleteColumn } from "../api/deleteColumn";
 
 interface ColumnProps {
   id: string;
@@ -16,6 +18,7 @@ interface ColumnProps {
 
 export default function Column({ id, title }: ColumnProps) {
   const [isEditing, setIsEditing] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
   const titleRef = useRef<HTMLInputElement>(null);
   const queryClient = useQueryClient();
   const MOCK_CARDS: CardType[] = [];
@@ -25,6 +28,13 @@ export default function Column({ id, title }: ColumnProps) {
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["columns"] });
       setIsEditing(false);
+    },
+  });
+
+  const deleteMutation = useMutation({
+    mutationFn: () => deleteColumn({ id }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["columns"] });
     },
   });
 
@@ -52,28 +62,50 @@ export default function Column({ id, title }: ColumnProps) {
   }, [isEditing]);
 
   return (
-    <Styled.ColumnContainer>
-      <Styled.ColumnHeader>
-        <Styled.TitleForm onSubmit={handleSubmitTitle}>
-          {isEditing ? (
-            <Styled.Title ref={titleRef} defaultValue={title} onBlur={handleBlur} />
-          ) : (
-            <span onClick={() => setIsEditing(true)}>{title}</span>
-          )}
-        </Styled.TitleForm>
-        <button onClick={() => setIsEditing(true)}>
-          <FaRegEdit />
-        </button>
-        <button>
-          <FaTrash />
-        </button>
-      </Styled.ColumnHeader>
-      <Styled.ColumnContent>
-        {MOCK_CARDS.map((card) => (
-          <Card key={card.id} {...card} />
-        ))}
-        <AdditionCardButton emptyColumn={MOCK_CARDS.length === 0} onClick={() => {}} />
-      </Styled.ColumnContent>
-    </Styled.ColumnContainer>
+    <>
+      <Styled.ColumnContainer>
+        <Styled.ColumnHeader>
+          <Styled.TitleForm onSubmit={handleSubmitTitle}>
+            {isEditing ? (
+              <Styled.Title ref={titleRef} defaultValue={title} onBlur={handleBlur} />
+            ) : (
+              <span onClick={() => setIsEditing(true)}>{title}</span>
+            )}
+          </Styled.TitleForm>
+          <button onClick={() => setIsEditing(true)}>
+            <FaRegEdit />
+          </button>
+          <button onClick={() => setModalOpen(true)}>
+            <FaTrash />
+          </button>
+        </Styled.ColumnHeader>
+        <Styled.ColumnContent>
+          {MOCK_CARDS.map((card) => (
+            <Card key={card.id} {...card} />
+          ))}
+          <AdditionCardButton emptyColumn={MOCK_CARDS.length === 0} onClick={() => {}} />
+        </Styled.ColumnContent>
+      </Styled.ColumnContainer>
+      <Modal
+        title="Delete column"
+        open={modalOpen}
+        onClose={() => setModalOpen(false)}
+        buttons={[
+          <button
+            onClick={() => {
+              deleteMutation.mutate();
+              setModalOpen(false);
+            }}
+          >
+            Confirm
+          </button>,
+          <button onClick={() => setModalOpen(false)}>Cancel</button>,
+        ]}
+      >
+        <span>컬럼 삭제시 컬럼에 있는 카드들도 모두 삭제됩니다.</span>
+        <br />
+        <span>컬럼을 정말 삭제하시겠습니까?</span>
+      </Modal>
+    </>
   );
 }
